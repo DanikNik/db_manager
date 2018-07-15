001c3ddb04ea54fd161eff11bba4ddb90629ed0f
DanikNik <nikolsky.dan@gmail.com>
2018-07-15 16:16:24 +1000

    added database class

---
+++ src/com/company/Database.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+
+public class Database {
+    private String databaseURL = "jdbc:mysql://127.0.0.1:3306/test";
+    private Connection con;
+
+    public Database() throws SQLException{
+        con = DriverManager.getConnection(databaseURL, "root", "sway9009");
+        con.setAutoCommit(false);
+    }
+
+    public void execute(String _query) throws SQLException{
+        Statement stm = con.createStatement();
+        stm.execute(_query);
+        stm.close();
+    }
+
+    public void show(String _tabName) throws SQLException{
+        Statement stm = con.createStatement();
+        ResultSet res = stm.executeQuery("SELECT * FROM " + _tabName);
+        ResultSetMetaData resMeta = res.getMetaData();
+        while (res.next()){
+            System.out.println(res.getObject(1) + ": " + res.getObject(2));
+        }
+    }
+
+    public void commit() throws SQLException{
+        con.commit();
+    }
+
+    public void close() throws SQLException{
+        con.close();
+    }
+
+    public void clearTable(String _tabName) throws SQLException{
+        Statement stm = con.createStatement();
+        stm.execute("TRUNCATE " + _tabName);
+        stm.close();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,41 @@ package com.company;
 import java.sql.*;
 
 import com.mysql.cj.jdbc.Blob;
+import com.company.Database;
 public class Main {
 
     public static void main(String[] args) throws SQLException{
-        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "user", "password");
-        con.setAutoCommit(false);
-        Statement stm = con.createStatement();
-//        stm.executeUpdate("create table tb1 (id integer primary key auto_increment, name varchar(255), data blob DEFAULT null)");
-//        stm.execute("insert into tb1 (name) values ('123'), ('abc'), ('xyz')");
-        ResultSet res = stm.executeQuery("select * from tb1");
-        while (res.next()){
-            System.out.println(res.getInt(1) + "^" + res.getString(2));
-        }
-        con.commit();
+
+        Database db = new Database();
+        db.execute("insert into table1 (name) values ('saxon'), ('roman'), ('nordic')");
+        db.commit();
+        db.show("table1");
+        db.clearTable("table1");
+        db.commit();
+        db.close();
+//        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "user", "password");
+//        con.setAutoCommit(false);
+//        Statement stm = con.createStatement();
+////        stm.executeUpdate("create table tb1 (id integer primary key auto_increment, name varchar(255), data blob DEFAULT null)");
+////        stm.execute("insert into tb1 (name) values ('123'), ('abc'), ('xyz')");
+//        ResultSet res = stm.executeQuery("select * from tb1");
+//        while (res.next()){
+//            System.out.println(res.getInt(1) + "^" + res.getString(2));
+//        }
+//        con.commit();
+//
+//
+//        PreparedStatement pst = con.prepareStatement("insert into tb1 (name) values (?), (?), (?)");
+//        pst.setString(1, "111");
+//        pst.setString(2, "222");
+//        pst.setString(3, "333");
+//        pst.execute();
+//        con.commit();
 
 
-        PreparedStatement pst = con.prepareStatement("insert into tb1 (name) values (?), (?), (?)");
-        pst.setString(1, "111");
-        pst.setString(2, "222");
-        pst.setString(3, "333");
-        pst.execute();
-        con.commit();
+
+
+
+
     }
 }
